fa198c930809da0f6784066c808752aae3c01744
zxwei <1649201921@qq.com>
2019-01-08 00:23:43 +0800

    添加小汽车，并可以做匀速直线运动

---
+++ assets/Script/car.js
@@ -0,0 +1,34 @@
+// Learn cc.Class:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/class.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/class.html
+// Learn Attribute:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        //最大速度
+        maxSpeed: 0,
+        //开始速度
+        startSpeed: 0,
+        //当前方向，弧度制
+        direction: 0,
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {},
+
+    start() {
+
+    },
+
+    update(dt) {
+        this.node.y += this.startSpeed * dt;
+    },
+});
--- assets/Script/game.js
+++ assets/Script/game.js
@@ -2,17 +2,20 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-        label: {
+        outRect: {
             default: null,
-            type: cc.Label
+            type: cc.Node
         },
-        // defaults, set visually when attaching this script to the Canvas
-        text: 'Hello, World!'
+        innerRect: {
+            default: null,
+            type: cc.Node
+        }
     },
 
     // use this for initialization
     onLoad: function () {
-        this.label.string = this.text;
+        
+        
     },
 
     // called every frame
